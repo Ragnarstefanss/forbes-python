087c1b8cbc5e905d571e526b2ad693dc491b7010
Ragnarstefanss <ragnars15@ru.is>
2020-07-19 10:58:16 +0000

    Renamed columns, sliced columns and started a list of possible functions to use

--- forbes.py
+++ forbes.py
@@ -6,11 +6,32 @@ import numpy as np
 import pandas as pd
 import urllib.request, json 
 import pandas as pd 
+import warnings
+warnings.filterwarnings('ignore')
+pd.set_option('display.max_columns',25)
 
 with urllib.request.urlopen("https://forbes400.herokuapp.com/api/forbes400?limit=10") as url:
     data = json.loads(url.read().decode())
-    #print(json.dumps(data, indent=4))
 
 df = pd.json_normalize(data)
+df.drop(['name'], axis=1, inplace=True)
+
+df.rename(columns = {'personName':'name'}, inplace=True)
+df.rename(columns = {'finalWorth':'net_worth'}, inplace=True)
 print(df.info())
-#print(df['personName'] + ' rank ' +  str(df['rank']) + ' net worth ' + str(df['finalWorth']))
+columns_main =  ['rank', 'name', 'net_worth', 'countryOfCitizenship', 'source', 'gender', 'financialAssets', 'estWorthPrev', 'privateAssetsWorth']
+print (df[columns_main])
+
+
+#print("Search by \n",
+ #     "1. Billions -> search_billions(<value>) \n",
+ #     "2. Country -> seach_country(<country name>) \n"
+ #     "3. Gender -> search_gender(< m | f > \n"
+ #     "4. Persons Assets -> assets(<persons name>, :number_of_shares)"
+ #     "\n"
+ #     " <> = replace with value, \n"
+ #     " : = optinal argument \n "
+#)
+
+#def search_billions(n):
+    
