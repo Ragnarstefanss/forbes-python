1635e65846b54134b6e120c96785f04463b8827b
Ragnarstefanss <ragnars15@ru.is>
2020-07-19 15:03:20 +0000

    Creatd a helper function for 'call_search_billionaire' and for 'call_assets_person_name' to get rid of repeating code

--- functions.py
+++ functions.py
@@ -6,7 +6,8 @@ Created on Sun Jul 19 14:26:06 2020
 """
 
 columns_main =  ['rank', 'name', 'net_worth', 'countryOfCitizenship', 'source', 'gender', 'financialAssets', 'estWorthPrev', 'privateAssetsWorth']
-
+columns_financial_assets = ['financialAssets']
+# ----------------------------------------------------------- #
 ## // Start: Assistant functions
 
 def capitalize_first_letter_in_each_word(user_input):
@@ -16,61 +17,51 @@ def capitalize_first_letter_in_each_word(user_input):
         new_string += val.capitalize()+ ' '
     return new_string.strip()
 
-## // End: Assistant functions
-    
 
-
-## // Start: call functions
-def call_search_billionaire(df):
+def search_by_name(df, call_type):
     while True:
         search_person = input("Enter persons name: ")
         new_name = capitalize_first_letter_in_each_word(search_person)
-        df_to_search2 = (df[columns_main])
-        match = df_to_search2[df['name'].str.match(new_name)]
+        df_to_search = (df[columns_main])
+        match = df_to_search[df['name'].str.match(new_name)]
         try:
             if not match.empty:
-                print(match)
+                if(call_type == "search_billionaire"):
+                    print(match)
+                elif(call_type == "search_assets"):
+                    print(match[['financialAssets']])
                 break
             else:
                 print("No person by that name")
-                
                 continue
         except ValueError:
             print ("call_assets_persons_name ValueError")
-            continue  
+            continue 
+## // End: Assistant functions
+# ----------------------------------------------------------- #
+    
+## // Start: call functions
+def call_search_billionaire(df):
+    search_by_name(df, "search_billionaire")
 
 def call_search_billions(df):
    billions_value = input("Enter amount in billions: ")
    search_billions(df, billions_value)
    
 def call_assets_person_name(df):
-    while True:
-        assets_persons_name = input("Enter persons name: ")
-        new_name = capitalize_first_letter_in_each_word(assets_persons_name)
-        df_to_search2 = (df[columns_main])
-        match = df_to_search2[df['name'].str.match(new_name)]
-        try:
-            if not match.empty:
-                print(df_to_search2[['financialAssets']])
-                break
-            else:
-                print("No person by that name")
-                
-                continue
-        except ValueError:
-            print ("call_assets_persons_name ValueError")
-            continue   
+    search_by_name(df, "search_assets") 
 
 def call_search_country(df):
     return 0
 
 ## // End: call functions
-
-## START: Main function
+# ----------------------------------------------------------- #
+    
+## // START: Main function
 
 # 2. More than <value> Billions + -> search_billions(<value>)
 def search_billions(df, value):
     new_value = int(value) * 1000
     print(df[df['net_worth'] > new_value][columns_main])
 
-## END: Main function
+## // END: Main function
