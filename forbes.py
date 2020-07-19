052e2da70347b3a8cd4313dd791e224f5628b8a7
Ragnarstefanss <ragnars15@ru.is>
2020-07-19 11:40:17 +0000

    Added a menu to call functions, implemented 'search by + billions', *functions have NOT been error checked, this is a very basic version*

--- forbes.py
+++ forbes.py
@@ -18,20 +18,46 @@ df.drop(['name'], axis=1, inplace=True)
 
 df.rename(columns = {'personName':'name'}, inplace=True)
 df.rename(columns = {'finalWorth':'net_worth'}, inplace=True)
-print(df.info())
+#print(df.info())
 columns_main =  ['rank', 'name', 'net_worth', 'countryOfCitizenship', 'source', 'gender', 'financialAssets', 'estWorthPrev', 'privateAssetsWorth']
-print (df[columns_main])
+#print (df[columns_main])
 
+## // Start: Functions
 
-#print("Search by \n",
- #     "1. Billions -> search_billions(<value>) \n",
- #     "2. Country -> seach_country(<country name>) \n"
- #     "3. Gender -> search_gender(< m | f > \n"
- #     "4. Persons Assets -> assets(<persons name>, :number_of_shares)"
- #     "\n"
- #     " <> = replace with value, \n"
- #     " : = optinal argument \n "
-#)
+# Options menu
+def options(): 
+    while True:
+        max_value = 5
+        print("Search by \n",
+              "\n",
+              "1. More than <value> Billions -> search_billions(<value>) \n",
+              "2. Country -> search_country(<country name>) \n"
+              "3. Gender -> search_gender(< m | f > \n"
+              "4. Persons Assets -> assets(<persons name>, :number_of_shares)\n"
+              "5. QUIT"
+        )
+        
+        user_input = int(input("Enter a number: "))
+        try:
+            #if(int(user_input) <= 0 or int(user_input) > max_value):
+                #print ('Enter a valid number')
+                #continue
+            if user_input == 1:
+                billions_value = input("Enter amount in billions: ")
+                search_billions(billions_value)
+            elif user_input == 5:
+                break
+        except ValueError:
+            print ('Enter a valid number')
+            continue
+        
+# 1 More than <value> Billions + -> search_billions(<value>)
+def search_billions(value):
+    new_value = int(value) * 1000
+    print(df[df['net_worth'] > new_value][columns_main])
 
-#def search_billions(n):
+## // END: functions
+
+# call options menu    
+options()
     
