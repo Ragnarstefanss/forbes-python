3f979f5169fd37ac795035da4baa0707d16fb0a4
Ragnarstefanss <ragnars15@ru.is>
2020-07-22 21:40:50 +0000

    UPDATED: structure of input menu in main file (forbes.py)

--- forbes.py
+++ forbes.py
@@ -21,47 +21,77 @@ with urllib.request.urlopen(url) as url:
     data = json.loads(url.read().decode())
 
 df_json = pd.json_normalize(data)
+#this was always set to the same value which was irrelevant so it got dropped
 df_json.drop(['name'], axis=1, inplace=True)
 
 df_json.rename(columns = {'personName':'name'}, inplace=True)
 df_json.rename(columns = {'finalWorth':'net_worth'}, inplace=True)
-df = df_json[:].fillna('')
-#print(df.info())
+df = df_json[:].fillna(0, inplace=True)
 
-# Options menu
-def options(): 
-    while True:
+#print(df.info())
+def valid_user_input(number, max_value):
+    if(int(number) <= 0 or int(number) > max_value):
+        print ('Enter a valid number')
+        return False
+    else:
+        return True
         
-        print("\n Search by \n",
-              "\n",
-              "1. Search billionaire by name -> search_billionaire(<persons name>) \n"
-              "2. More than <value> Billions -> search_billions(<value>) \n",
-              "3. Country -> search_country(<country name>) \n"
-              "4. Gender -> search_gender(< m | f > \n"
-              "5. Persons Assets -> assets(<persons name>, :number_of_shares)\n"
-              "6. Create a histogram\n"
-              "7. QUIT"
+
+def search_options():
+    while True:
+        print("\n Search billionaires by \n",
+              "1. Name\n",
+              "2. Gender\n"
+              "3. Networth more than <value> billions\n",
+              "4. Country citicenship\n",
+              "5. EXIT (Go back to main menu)\n"
         )
-        max_value = 7
+        max_value = 5
         user_input = int(input("Enter a number: "))
         try:
-            if(int(user_input) <= 0 or int(user_input) > max_value):
-                print ('Enter a valid number')
+            if(valid_user_input(user_input, max_value) != True):
                 continue
             if user_input == 1:
+                #1. Name
                 call_search_billionaire(df)
             if user_input == 2:
-                call_search_billions(df)
+                #2. Gender
+                continue
             if user_input == 3:
-                call_search_country(df)
-                continue
+                #3. Net worh over <X> billions
+                call_search_billions(df)
             if user_input == 4:
+                #4. Contry
+                call_search_country(df)
+            elif user_input == max_value:
+                break
+        except ValueError:
+            print ('Enter a valid number')
+            continue
+
+# Options menu
+def main_options(): 
+    while True:
+        print("1. Search billionaires\n",
+              "2. Show billionaires investment assets\n",
+              "3. Create graphs\n",
+              "4. Quit\n",
+        )
+        max_value = 4
+        user_input = int(input("Enter a number: "))
+        try:
+            if(valid_user_input(user_input, max_value) != True):
                 continue
-            if user_input == 5:
-                call_assets_person_name(df)
-            if user_input == 6:
+            if user_input == 1:
+                #1. Search billionaires -> menu for search
+                search_options()
+            if user_input == 2:
+                #2. Investment assets
+                call_assets_person_name(df)(df)
+            if user_input == 3:
+                #3. Create plots
                 call_create_plot(df)
-            elif user_input == 7:
+            elif user_input == max_value:
                 break
         except ValueError:
             print ('Enter a valid number')
@@ -71,5 +101,5 @@ def options():
 ## // END: Main functions
 
 # call options menu    
-options()
+main_options()
     
